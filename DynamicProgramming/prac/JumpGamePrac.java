7c7ccaab84fd46f14c63e691e61c41ed8410c81c
Abner Rodriguez <abner07282005@gmail.com>
2025-10-04 10:08:59 -0400

    Leetcode #300 Longest Increasing Subsequence

---
+++ DynamicProgramming/prac/JumpGamePrac.java
@@ -0,0 +1,17 @@
+package DynamicProgramming.prac;
+
+public class JumpGamePrac {
+
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int target = n -1;
+
+        for(int i = n-1; i >= 0; i--) {
+            if(i + nums[i] >= target) {
+               target = i;
+            }
+
+        }
+        return (target == 0);
+    }
+}
